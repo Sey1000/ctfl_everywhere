ea3153712f1fe6bd4c95d92347d593fda8634e62
Sey1000 <22985121+Sey1000@users.noreply.github.com>
2018-03-13 17:07:05 +0100

    Set client inside controller

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,2 +1,13 @@
 class MissionsController < ApplicationController
+  before_action: :set_client
+
+  private
+
+  def set_client
+    @client ||= Contentful::Client.new(
+      space: ENV['SPACE_ID'],
+      access_token: ENV['CONTENT_DELIVERY_API_TOKEN'],
+      dynamic_entries: :auto
+    )
+  end
 end
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,14 +1,3 @@
 class Mission < ApplicationRecord
 
-  private
-
-  def client
-    @client ||= Contentful::Client.new(
-      space: ENV[SPACE_ID],
-      access_token: ENV[CONTENT_DELIVERY_API_TOKEN],
-      dynamic_entries: :auto
-      # raise_errors: true
-    )
-  end
-
 end
