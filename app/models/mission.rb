125de221889fd6944f92e6f2445ff2c3bb30ff27
Sey1000 <22985121+Sey1000@users.noreply.github.com>
2018-03-13 16:39:08 +0100

    Configure client

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,2 +1,14 @@
 class Mission < ApplicationRecord
+
+  private
+
+  def client
+    @client ||= Contentful::Client.new(
+      space: ENV[SPACE_ID],
+      access_token: ENV[CONTENT_DELIVERY_API_TOKEN],
+      dynamic_entries: :auto
+      # raise_errors: true
+    )
+  end
+
 end
