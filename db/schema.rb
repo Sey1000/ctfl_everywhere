ab0d6bad1d75ba744652a83594e3c9135990ff23
Sey1000 <22985121+Sey1000@users.noreply.github.com>
2018-03-13 18:45:36 +0100

    Add title to missions

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313152043) do
+ActiveRecord::Schema.define(version: 20180313174357) do
 
   create_table "missions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
